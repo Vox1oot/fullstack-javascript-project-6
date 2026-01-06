9132eeadd2c4630e88c592c6ef5a2d092fe949d3
Vitaliy Morzhov <voxfoot@gmail.com>
2026-01-06 12:17:51 +0500

    feat: add .npmrc configuration for Node options

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-import 'dotenv/config'
-import Fastify from 'fastify'
-import plugin from '../server/plugin.js'
-
-const fastify = Fastify({
-  logger: true,
-})
-
-await fastify.register(plugin)
-
-const port = process.env.PORT || 3000
-const host = process.env.HOST || '0.0.0.0'
-
-try {
-  await fastify.listen({ port, host })
-}
-catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
-}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
