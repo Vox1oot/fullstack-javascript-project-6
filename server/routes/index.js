2ab83ff5990c92f1075343544b406e33a6badf47
Vitaliy Morzhov <voxfoot@gmail.com>
2026-01-07 15:48:24 +0500

    feat: implement routing for welcome, users, session, statuses, tasks, and labels

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+import welcome from './welcome.js'
+import users from './users.js'
+import session from './session.js'
+import statuses from './statuses.js'
+import tasks from './tasks.js'
+import labels from './labels.js'
+
+const routes = [
+  welcome,
+  users,
+  session,
+  statuses,
+  tasks,
+  labels,
+]
+
+export default (app) => {
+  routes.forEach(route => route(app))
+}
