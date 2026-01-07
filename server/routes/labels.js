1b3e2136fe98ab014ef5f81cc794839c672e3269
Vitaliy Morzhov <voxfoot@gmail.com>
2026-01-07 16:53:00 +0500

    feat: replace createCRUDRoutes with explicit CRUD operations for labels and statuses

--- server/routes/labels.js
+++ server/routes/labels.js
@@ -1,13 +1,73 @@
-import { createCRUDRoutes } from '../lib/crudRoutes.js'
+import i18next from 'i18next'
 import { FLASH_KEYS } from '../constants/flashKeys.js'
 
 export default (app) => {
-  createCRUDRoutes(app, 'label', 'labels', {
-    createSuccess: FLASH_KEYS.labels.create.success,
-    createError: FLASH_KEYS.labels.create.error,
-    updateSuccess: FLASH_KEYS.labels.update.success,
-    updateError: FLASH_KEYS.labels.update.error,
-    deleteSuccess: FLASH_KEYS.labels.delete.success,
-    deleteError: FLASH_KEYS.labels.delete.error,
-  })
+  const objectionModels = app.objection.models
+
+  app
+    .get('/labels', { preValidation: app.authenticate }, async (request, reply) => {
+      const labels = await objectionModels.label.query()
+      reply.render('labels/index', { labels })
+      return reply
+    })
+    .get('/labels/new', { preValidation: app.authenticate }, (request, reply) => {
+      const label = new objectionModels.label()
+      reply.render('labels/new', { label })
+      return reply
+    })
+    .get('/labels/:id/edit', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const label = await objectionModels.label.query().findById(id)
+      reply.render('labels/edit', { label })
+      return reply
+    })
+    .post('/labels', { preValidation: app.authenticate }, async (request, reply) => {
+      const label = new objectionModels.label()
+      label.$set(request.body.data)
+
+      try {
+        await objectionModels.label.query().insert(request.body.data)
+        request.flash('info', i18next.t(FLASH_KEYS.labels.create.success))
+        reply.redirect('/labels')
+      }
+      catch ({ data }) {
+        request.flash('error', i18next.t(FLASH_KEYS.labels.create.error))
+        reply.render('labels/new', { label, errors: data })
+      }
+      return reply
+    })
+    .patch('/labels/:id', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const label = await objectionModels.label.query().findById(id)
+      const { data } = request.body
+
+      try {
+        label.$set(data)
+        await label.$query().patch(data)
+        request.flash('info', i18next.t(FLASH_KEYS.labels.update.success))
+        reply.redirect('/labels')
+      }
+      catch (errors) {
+        request.flash('error', i18next.t(FLASH_KEYS.labels.update.error))
+        const errorData = errors.data || errors
+        reply.render('labels/edit', { label, errors: errorData })
+      }
+      return reply
+    })
+    .delete('/labels/:id', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const label = await objectionModels.label.query().findById(id)
+
+      try {
+        await label.$query().delete()
+        request.flash('info', i18next.t(FLASH_KEYS.labels.delete.success))
+        reply.redirect('/labels')
+      }
+      catch (error) {
+        console.error(error)
+        request.flash('error', i18next.t(FLASH_KEYS.labels.delete.error))
+        reply.redirect('/labels')
+      }
+      return reply
+    })
 }
--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -1,13 +1,73 @@
-import { createCRUDRoutes } from '../lib/crudRoutes.js'
+import i18next from 'i18next'
 import { FLASH_KEYS } from '../constants/flashKeys.js'
 
 export default (app) => {
-  createCRUDRoutes(app, 'status', 'statuses', {
-    createSuccess: FLASH_KEYS.statuses.create.success,
-    createError: FLASH_KEYS.statuses.create.error,
-    updateSuccess: FLASH_KEYS.statuses.update.success,
-    updateError: FLASH_KEYS.statuses.update.error,
-    deleteSuccess: FLASH_KEYS.statuses.delete.success,
-    deleteError: FLASH_KEYS.statuses.delete.error,
-  })
+  const objectionModels = app.objection.models
+
+  app
+    .get('/statuses', { preValidation: app.authenticate }, async (request, reply) => {
+      const statuses = await objectionModels.status.query()
+      reply.render('statuses/index', { statuses })
+      return reply
+    })
+    .get('/statuses/new', { preValidation: app.authenticate }, (request, reply) => {
+      const status = new objectionModels.status()
+      reply.render('statuses/new', { status })
+      return reply
+    })
+    .get('/statuses/:id/edit', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const status = await objectionModels.status.query().findById(id)
+      reply.render('statuses/edit', { status })
+      return reply
+    })
+    .post('/statuses', { preValidation: app.authenticate }, async (request, reply) => {
+      const status = new objectionModels.status()
+      status.$set(request.body.data)
+
+      try {
+        const validStatus = await objectionModels.status.fromJson(request.body.data)
+        await objectionModels.status.query().insert(validStatus)
+        request.flash('info', i18next.t(FLASH_KEYS.statuses.create.success))
+        reply.redirect('/statuses')
+      }
+      catch ({ data }) {
+        request.flash('error', i18next.t(FLASH_KEYS.statuses.create.error))
+        reply.render('statuses/new', { status, errors: data })
+      }
+      return reply
+    })
+    .patch('/statuses/:id', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const status = await objectionModels.status.query().findById(id)
+      const { data } = request.body
+
+      try {
+        await status.$query().patch(data)
+        request.flash('info', i18next.t(FLASH_KEYS.statuses.update.success))
+        reply.redirect('/statuses')
+      }
+      catch (errors) {
+        request.flash('error', i18next.t(FLASH_KEYS.statuses.update.error))
+        const errorData = errors.data || errors
+        reply.render('statuses/edit', { status, errors: errorData })
+      }
+      return reply
+    })
+    .delete('/statuses/:id', { preValidation: app.authenticate }, async (request, reply) => {
+      const { id } = request.params
+      const status = await objectionModels.status.query().findById(id)
+
+      try {
+        await status.$query().delete()
+        request.flash('info', i18next.t(FLASH_KEYS.statuses.delete.success))
+        reply.redirect('/statuses')
+      }
+      catch (error) {
+        console.error(error)
+        request.flash('error', i18next.t(FLASH_KEYS.statuses.delete.error))
+        reply.redirect('/statuses')
+      }
+      return reply
+    })
 }
